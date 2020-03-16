009a7986c42bc204d3e2aec45a155074f77ad05e
rhageboeck <32132880+rhageboeck@users.noreply.github.com>
2020-03-15 23:46:59 -0400

    Working bitmap drawing page

--- bitmap.js
+++ bitmap.js
@@ -1,29 +1,71 @@
 // MARK: Constants
-let number = 8;
+let number = 16;
+var currColor = "white";
+let supportedColors = ["red","orange","green","blue","lightblue","purple","white"];
+
+function clearGrid() {
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+            bitmap.rows[i].cells[j].style.background = "white";
+        }
+    }
+}
+
+function clickedBox(i,j) {
+    bitmap.rows[i].cells[j].style.background = currColor;
+}
 
 // MARK: Page Header
 var pageHeader = document.createElement("header");
 var title = document.createElement("h1");
 title.innerHTML = "Bitmap Generator";
-title.classList.add(["mb-4"]);
+title.classList.add("mb-4");
 title.style = "text-align:center;";
 pageHeader.appendChild(title);
 
-
-
 document.body.appendChild(pageHeader);
-
-
-
 
 
 // MARK: Page Body
 var pageBody = document.createElement("div");
 pageBody.id = "pageBody";
-pageBody.className = "container";
+pageBody.classList.add("container","mb-4");
+pageBody.appendChild(document.createElement("hr"));
 
-var table = document.createElement("table");
-table.classList.add(["mx-auto"]);
+// MARK: BITMAP MENU
+var menu = document.createElement("div");
+menu.classList.add("mx-auto","mb-3");
+
+// GENERAL COLOR BUTTONS
+for (let i = 0; i < supportedColors.length; i++) {
+    var btn = document.createElement("button");
+    btn.innerHTML = supportedColors[i].toUpperCase();
+    btn.style.background = supportedColors[i];
+    btn.classList.add("menuButton","col-sm-2","mb-2");
+    btn.onclick = function() {
+        currColor = supportedColors[i];
+    }
+    if (supportedColors[i] == "white") {
+        btn.style.color = "black";
+    }
+    menu.appendChild(btn);
+}
+// SPECIAL BUTTONS
+var clrbtn = document.createElement("button");
+clrbtn.innerHTML = "CLEAR";
+clrbtn.onclick = function() {
+    clearGrid();
+}
+clrbtn.classList.add("menuButton","specialButtons","col-md-2");
+menu.appendChild(clrbtn);
+
+pageBody.appendChild(menu);
+
+
+
+// MARK: BITMAP
+var bitmap = document.createElement("table");
+bitmap.classList.add("mx-auto");
 for (let i = 0; i < number; i++) {
     var row = document.createElement("tr");
     for (let j = 0; j < number; j++) {
@@ -31,12 +73,15 @@ for (let i = 0; i < number; i++) {
         if (j < number-1) {
             cell.classList.add("innercell");
         }
+        cell.onclick = function () {
+            clickedBox(i,j);
+        }
         // cell.innerHTML = "hello";
         row.appendChild(cell);
     }
-    table.appendChild(row);
+    bitmap.appendChild(row);
 }
-pageBody.appendChild(table);
+pageBody.appendChild(bitmap);
 
 
 document.body.appendChild(pageBody);
@@ -48,7 +93,8 @@ document.body.appendChild(pageBody);
 // MARK: Page Footer
 var pageFooter = document.createElement("footer");
 var copyright = document.createElement("p");
-copyright.innerHTML = "&copy;Rob Hageboeck 2020";
+copyright.innerHTML = "&copy; Rob Hageboeck 2020";
+copyright.style = "text-align:center;"
 pageFooter.appendChild(copyright);
 
 
